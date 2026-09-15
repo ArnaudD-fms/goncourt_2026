51f42d4a5d88bd8015407a981da30f8995669526
Arnaud Delperier <arnaud.delperier@fms-ea.com>
2026-09-15 09:28:38 +0200

    Ajout de la propriété book

--- models/author.py
+++ models/author.py
@@ -4,6 +4,7 @@
 Classe author
 """
 from dataclasses import dataclass, field
+from models.book import Book
 
 
 @dataclass
@@ -11,8 +12,8 @@ class Author:
     id: int | None = field(default=None, init=False)
     first_name: str
     last_name: str
-    biography: str
-    # book: Book = field(default=None, init=False)
+    biography: str = field(default=None, init=False)
+    book: Book = field(default=None, init=False)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
