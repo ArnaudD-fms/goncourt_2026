313f567cd43a0a055c637ca32793fb6170307a54
Arnaud Delperier <arnaud.delperier@fms-ea.com>
2026-09-15 10:30:09 +0200

    Ajout du model main_character

---
+++ models/main_character.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe main_character
+"""
+from dataclasses import dataclass, field
+
+
+@dataclass
+class main_character:
+    """
+    Classe réprésentant un personnage principal d'un livre
+
+    * id    : clé primaire de l'entité en base
+    * name  : nom du personnage
+    """
+    id: int | None = field(default=None, init=False)
+    name: str
+
+    def __str__(self):
+        return self.name
