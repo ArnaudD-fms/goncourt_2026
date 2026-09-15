9bbb6b9038664211c51d4095b2de3af12abe435c
Arnaud Delperier <arnaud.delperier@fms-ea.com>
2026-09-15 15:09:37 +0200

    Ajout de la méthode pour créer les associations entre les selections et les livres dans book_dao

--- daos/book_dao.py
+++ daos/book_dao.py
@@ -1,5 +1,7 @@
 from typing import List
 
+import pymysql
+
 from daos.dao import Dao
 from models.author import Author
 from models.book import Book
@@ -7,7 +9,14 @@ from models.publisher import Publisher
 
 
 class BookDao(Dao[Book]):
+
     def read_book_by_selection(self, selection_number: int) -> List[Book]:
+        """
+        Récupère les livres correspondant à une sélection
+
+        :param selection_number: le numéro de la sélection dont on souhaite récupérer les livres
+        :return: la liste des livres sélectionés
+        """
         with Dao.connection.cursor() as cursor:
             sql = """
                 SELECT * FROM go_book
@@ -35,3 +44,41 @@ class BookDao(Dao[Book]):
                 books.append(book)
 
             return books
+
+    def create_book_selection(self, selection_number: int, books: List[Book]) -> None:
+        """
+        Créer en base les associations entre les livres et les sélections
+
+        :param selection_number: le numéro de la sélection à laquelle on souhaite associer les livres
+        :param books: la liste des livres à ajouter à la sélection
+        """
+        try:
+            with Dao.connection.cursor() as cursor:
+                # Récupération de l'id de la sélection depuis son numéro
+                sql = """
+                    SELECT se_id_selection FROM go_selection WHERE se_number = %s
+                """
+                cursor.execute(sql, (selection_number,))
+                id_selection = cursor.fetchone()
+
+                # Création la liste des association livre / sélection que l'on souhaite insérer en base
+                values = ", ".join(
+                    f"({book.id}, "
+                    f"{id_selection["se_id_selection"]})"
+                    for book in books
+                )
+
+                # Execution de la réquête en passant les valeurs préparées
+                sql = f"""
+                    INSERT INTO go_book_selection (bs_id_book, bs_id_selection) VALUES {values};
+                """
+                cursor.execute(sql, values)
+
+            # TODO pour la review avec les formateurs :
+            #  est-ce une bonne approche ? (SELECT sur la table selection depuis le dao de book)
+            #  est-ce que ce code respecte les bonnes pratiques ?
+            Dao.connection.commit()
+
+        except pymysql.MySQLError as e:
+            Dao.connection.rollback()
+            print(f"Erreur SQL : {e}")
