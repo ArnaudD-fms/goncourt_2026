a3db5790890d118d97e09e9e6ea1ba3a354e8611
Arnaud Delperier <arnaud.delperier@fms-ea.com>
2026-09-16 12:04:53 +0200

    Ajout des docstring dans goncourt.py ainsi que la méthode ast_to_vote

--- business/goncourt.py
+++ business/goncourt.py
@@ -1,28 +1,12 @@
 from typing import ClassVar, List
+
+from daos import book_dao
 from daos.book_dao import BookDao
 from daos.jury_dao import JuryDao
 from models.book import Book
 
 
 class Goncourt:
-    # Mettre en place un menu ou l'utilisateur pourra choisir l'action entrant le numero correspondant
-
-    # Demander si on est utilisateur ou le president
-    # si utilisateur choix 1 et 2 uniquement
-    # si président tous les choix
-
-    # 1 - Afficher la liste du jury
-    # 2 - Afficher la liste des livres d'une selection
-    #       2.1 - Demander la selection (entre 1 et 3) -> si date jour inferieur a date selection prevenir l'utilisateur
-    # 3 - Indiquer les livres d'une selection
-    #       3.1 - Demander la selection
-    #       3.2 - Afficher les id des livres.
-    #       3.3 - Demander d'entrer les id des livres à ajouter
-    # 4 - Voter pour les livres
-    #       4.1 Affiche directement les livres de la 3eme selection
-    #       4.2 Demander de choisir un livre ne rentrant l'id
-    #       4.3 Demander le nombre de votes à ajouter
-
     END_OF_PROGRAM_MESSAGE: ClassVar[str] = "Au revoir !"
 
     profile: str = "undefined"
@@ -32,6 +16,13 @@ class Goncourt:
     books: list[Book] = []
 
     def get_valid_input(self, max_choice: int, min_choice: int = 1):
+        """
+        Permet de vérifier si l'input saisi par l'utilisateur est valide
+
+        :param max_choice: le choix max accepté dans les actions proposées
+        :param min_choice: le choix min accepté dans les actions proposées
+        :return: le choix de l'utilisateur sous forme d'int
+        """
         valid_input = False
         user_input = ""
         while not valid_input:
@@ -49,6 +40,10 @@ class Goncourt:
         return int(user_input)
 
     def ask_user_profile(self):
+        """
+        Demande à l'utilisateur de choisir son profil ou permet de quitter le programme
+
+        """
         print("Etes vous un simple utilisateur ou bien le GRAND et VENERABLE président de l'académie goncourt ?")
         print(" 1 - Simple utilisateur")
         print(" 2 - Vénérable président")
@@ -66,9 +61,12 @@ class Goncourt:
                 print(self.END_OF_PROGRAM_MESSAGE)
 
     def ask_main_actions(self):
+        """
+        Demande à l'utilisateur l'action qu'il souhaite effectuer en fonction de son profile
+
+        """
 
         user_action = 0
-
         print("Que souhaitez-vous faire ?")
         print(" 1 - Afficher le jury")
         print(" 2 - Afficher les livres d'une sélection")
@@ -85,48 +83,86 @@ class Goncourt:
         match user_action:
 
             case 1:
-                juries = self.jury_dao.read_all()
-                for jury in juries:
-                    print(jury)
+                self.display_juries()
 
             case 2:
-                selection = self.ask_selection()
-                books = self.book_dao.read_book_by_selection(selection)
-                for book in books:
-                    print(book)
+                self.display_books()
 
             case 3:
                 if self.profile == "president":
-                    selection_to_update = self.ask_selection_to_update()
-                    self.books = self.book_dao.read_book_by_selection(selection_to_update-1)
-                    books_to_add: List[Book] = []
-                    continue_adding_book = True
-                    while continue_adding_book:
-                        self.ask_book_to_add(self.books, books_to_add)
-                        continue_adding_book = self.ask_continue_adding_books()
-
-                    self.book_dao.create_book_selection(selection_to_update, books_to_add)
+                    self.update_selection()
 
                 elif self.profile == "user":
                     self.end_program = True
                     print(self.END_OF_PROGRAM_MESSAGE)
 
             case 4:
-                print("self.vote()")
+                self.ask_to_vote()
 
             case 5:
                 self.end_program = True
                 print(self.END_OF_PROGRAM_MESSAGE)
 
+    def update_selection(self):
+        """
+        Demande à l'utilisateur de mettre à jour les livres d'une sélection
+
+        """
+
+        # Récupère la sélection que l'utilisateur souhaite mettre à jour
+        selection_to_update = self.ask_selection_to_update()
+
+        # Récupère la liste des livres pouvant être choisi pour la sélection que l'utilisateur suhaite mettre à jour
+        self.books = self.book_dao.read_book_by_selection(selection_to_update - 1)
+
+        # Demande à l'utilisateur de choisir les livres qu'il souhaite ajouter à la sélection
+        books_to_add: List[Book] = []
+        continue_adding_book = True
+        while continue_adding_book:
+            self.ask_book_to_add(self.books, books_to_add)
+            continue_adding_book = self.ask_continue_adding_books()
+
+        # Appel du DAO afin de mettre à jour la sélection en base
+        self.book_dao.create_book_selection(selection_to_update, books_to_add)
+        print(f"Livres ajoutés à la sélection n° {selection_to_update} : ")
+        for book in books_to_add:
+            print(f" - {book.title}")
+
+    def display_books(self):
+        """ Récupère et affiche les livres d'une sélection """
+        selection = self.ask_selection()
+        books = self.book_dao.read_book_by_selection(selection)
+        for book in books:
+            print("")
+            print(book)
+
+    def display_juries(self):
+        """ Récupère et affiche les membres du jury """
+        juries = self.jury_dao.read_all()
+        for jury in juries:
+            print(jury)
+
     def ask_selection(self) -> int:
+        """ Demande à l'utilisateur quelle sélection il souhaite afficher """
+        print("")
         print("Quel sélection souhaitez vous afficher ? (1, 2 ou 3)")
         return self.get_valid_input(3)
 
     def ask_selection_to_update(self) -> int:
+        """ Demande à l'utilisateur quelle sélection il souhaite mettre à jour """
+        print("")
         print("Quel sélection voulez-vous mettre à jour ? (2 ou 3)")
         return self.get_valid_input(3, 2)
 
     def ask_book_to_add(self, books, books_to_add):
+        """
+        Affiche une liste de livre et demande à l'utilisateur de choisir le livre à ajouter à la liste des livres qui
+        permettra de mettre à jour une sélection
+
+        :param books: une liste de livre correspondante à une sélection
+        :param books_to_add: la liste de livres permettant de mettre à jour une sélection
+        """
+        print("")
         for i, book in enumerate(books):
             print(f" - {i+1} : {book.title}")
 
@@ -141,6 +177,8 @@ class Goncourt:
                     print("Ce livre concourt déjà pour cette sélection")
 
     def ask_continue_adding_books(self):
+        """ Demande à l'utilisateur s'il souhaite ajouter d'autre livre à la sélection à mettre à jour"""
+        print("")
         print("Voulez-vous ajouter d'autres livres à la sélection ? ")
         print(" 1 - Oui")
         print(" 2 - Non")
@@ -151,3 +189,31 @@ class Goncourt:
             return True
         else:
             return False
+
+    def ask_to_vote(self):
+        """
+        Demande à l'utilisateur de choisir un livre parmi ceux couconrant dans la dernière sélection, et de choisir
+        un nombre de votes à lui ajouter
+        
+        """
+        books = self.book_dao.read_book_by_selection(3)
+
+        if len(books) == 0:
+            print("La dernère sélection n'a pas encore était établit. Le président doit sélectioner des livres avant"
+                  "de pouvoir ajouter les votes.")
+
+        else:
+            print("pour quel livre souhaitez-vous ajouter des votes ?")
+
+            for i, book in enumerate(books):
+                print(f" - {i+1} : {book.title}")
+
+            book_nbr = self.get_valid_input(len(books))
+
+            print("Combien de votes souhaitez-vous ajouter ?")
+
+            number_of_votes = self.get_valid_input(16)
+
+            for i, book in enumerate(books):
+                if book_nbr == i+1:
+                    self.book_dao.update_book_number_of_votes(book.id, number_of_votes)
--- main.py
+++ main.py
@@ -16,5 +16,6 @@ if __name__ == '__main__':
     goncourt = Goncourt()
     goncourt.ask_user_profile()
     while not goncourt.end_program:
+        print("")
         goncourt.ask_main_actions()
 
